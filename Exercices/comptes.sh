78ebc4b7974106fce6dc179ef233d5ea5486c711
Catalina Alvarez <catalinaalvarez@MacBookPro.lan>
2025-10-11 23:18:43 +0200

    tp1-ex1

---
+++ Exercices/comptes.sh
@@ -0,0 +1,11 @@
+
+bash-3.2$ ls
+2016	2017	2018
+bash-3.2$ grep -r "Location" 2016/ | wc -l
+    3144
+bash-3.2$ grep -r "Location" 2017/ | wc -l
+    2466
+bash-3.2$ grep -r "Location" 2018/ | wc -l
+    3110
+
+
