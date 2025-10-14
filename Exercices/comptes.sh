26140d7784a42a8ffd3a36647eccab50dc9d5f7a
Catalina Alvarez <catalinaalvarez@MacBookPro.lan>
2025-10-14 22:45:17 +0200

    update comptes.sh

--- Exercices/comptes.sh
+++ Exercices/comptes.sh
@@ -1,11 +1,19 @@
+#!/bin/bash
 
-bash-3.2$ ls
-2016	2017	2018
-bash-3.2$ grep -r "Location" 2016/ | wc -l
-    3144
-bash-3.2$ grep -r "Location" 2017/ | wc -l
-    2466
-bash-3.2$ grep -r "Location" 2018/ | wc -l
-    3110
+echo "argument donné" : $1
+
+
+
+CHEMIN="$HOME/cours/plurital/PPE1-2025/exercice_1/ann"
+
+echo "location 2016 :"
+grep -r "Location" $CHEMIN/2016 | wc -l
+
+echo "location 2017 :"
+cat $CHEMIN/2017/*/* | grep "Location" | wc -l
+
+echo "location 2018 :" 
+grep -r "Location" $CHEMIN/2018/ | wc -l
+    
 
 
